0f780cc7508143a6ad7925ccdda7c503bc108738
maxenceng <maxence.grosjean@cpe.fr>
2017-12-11 19:10:36 +0000

    Added the additional option on page

--- trello.js
+++ trello.js
@@ -4,17 +4,35 @@
  */
 function delegate(eventName, selector, fn) {
   document.addEventListener(eventName, (e) => {
-    $closest = $(e.target).closest(selector);
+    $closest = $(e.target).closest(selector)
     if ( $closest.length ) {
-      fn(e, $closest);
+      fn(e, $closest)
     }
 
   // Use capture phase
-  }, true);
+  }, true)
 }
 
-const test = (event, closest) => {
-  console.log(event, closest);
+/**
+ *
+ * @param {[type]} event
+ * @param {[type]} closest
+ */
+const addElem = (event, closest) => {
+  console.log(typeof event, typeof closest)
+  setTimeout(() => {
+    const selector = $('.pop-over-list')
+    selector.append(`
+      <li>
+        <a href="#" class="highlight-icon copy">Copy to clipboard</a>
+      </li>
+    `)
+  }, 1)
 }
 
-delegate('click', '.icon-overflow-menu-horizontal', test);
+const copyToClipboard = (event, closest) => {
+  console.log(event, closest)
+}
+
+delegate('click', '.list-header-extras', addElem)
+delegate('click', '.copy', copyToClipboard)
